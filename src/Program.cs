79fbbe2b0e6532c00675a9be15b189d8b311de9d
Zuwa Omigie <oomigie@zynga.com>
2017-06-26 14:46:31 -0400

    initial commit

---
+++ src/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace mainProgram
+{
+    class MainClass
+    {
+        public static void Main(string [] args)
+        {
+            Reporter r = new Reporter();
+
+            r.ShowFiles();
+             
+        }
+    }
+}
---
+++ src/Reporter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace mainProgram
+{
+    /*
+     * File System reporter 
+     * 
+     * Report format:
+     * - How many files of each type were encountered.
+     * - How much space each type takes up on disk.
+     * - Which file of each type took up the most space.
+     * 
+    */
+    public class Reporter
+    {
+
+        public Reporter()
+        {
+            Console.WriteLine("starting another one...");
+            try
+            {
+                string cwd = Directory.GetCurrentDirectory();
+
+                Console.WriteLine("cwd:" + cwd);
+
+
+                foreach(string path in Directory.EnumerateFiles(cwd))
+                {
+                    string[] file = path.Split('.');
+                    string fileExt = file[1];
+                    string [] tree = file[0].Split('/');
+                    string fileName = tree[tree.Length - 1];
+
+                    Console.Write("filename: " + fileName + "\t");
+                    Console.Write("extension: " + fileExt + "\n");
+                }
+                ;
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("OOPSY" + e);
+
+              
+            }
+        }
+
+        public void ShowFiles()
+        {
+            return;
+        }
+    }
+}
+
